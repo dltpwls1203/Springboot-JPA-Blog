f0b63907cfb34a839d8d64ae02bed37b0a1fad5e
SEJIN\sejin <dev021@4csoft.com>
2023-07-11 17:56:49 +0900

    20230711-2

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,6 +1,6 @@
 let index = {
 	init: function() {
-		$("#btn-save").on("click", ()=>{
+		$("#btn-save").on("click", ()=>{ // function() {}, ()=>{} this를 바인딩하기 위해서
 			this.save();
 		});
 	},
@@ -11,11 +11,29 @@ let index = {
 			username: $("#username").val(),
 			password: $("#password").val(),
 			email: $("#email").val()
-		}
+		};
 		
 		//console.log(data);
 		
-		$.ajax().done().fail(); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
+		// ajax 호출 시 default가 비동기 호출
+		// ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
+		$.ajax({
+			// 회원가입 수행 요청
+			type: "POST",
+			url: "/blog/api/user",
+			data: JSON.stringify(data), // http body 데이터
+			contentType: "application/json; charset=utf-8", // body데이터가 어떤 타입인지(MIME)
+			dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 문자열(String) (생긴게 json이라면) => javascript오브젝트로 변경
+		}).done(function(resp){
+			// 요청 성공
+			alert("회원가입이 완료되었습니다.");
+			console.log(resp);
+			location.href="/blog";
+		}).fail(function(error){
+			// 요청 실패
+			alert(JSON.stringify(error));
+		}); 
+
 	}
 }
 
--- src/main/webapp/WEB-INF/views/layout/header.jsp
+++ src/main/webapp/WEB-INF/views/layout/header.jsp
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 <title>Bootstrap Example</title>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 <meta charset="utf-8">
